af1dcdfa2b91eb96bf97144c70d59e4824a93395
ŚÏĐĐHÃŘŤH <57821872+Siddharthtewani@users.noreply.github.com>
2021-02-01 13:04:03 +0530

    Update NLPSolution.py

--- NLPSolution.py
+++ NLPSolution.py
@@ -15,7 +15,6 @@ df.shape
 # %%
 df.drop(["Unnamed: 2","Unnamed: 3","Unnamed: 4","Unnamed: 5","Unnamed: 6"],axis=1,inplace=True)
 # %%
-
 df.dropna(inplace=True)
 df.isnull().sum()
 # %%
@@ -39,24 +38,49 @@ password_tuple
 # %%
 import random
 random.shuffle(password_tuple)
-# %%
+# %%git 
 x=[label[0] for label in password_tuple]
 y=[label[1] for label in password_tuple]
-x
 # %%
-y
+def characters(input):
+    charac=[]
+    for i in input:
+        charac.append(i)
+    return charac
 # %%
+from sklearn.feature_extraction.text import TfidfVectorizer
+vector=TfidfVectorizer(tokenizer=characters)
+x=vector.fit_transform(x)
+# %%
+x.shape
+# %%
+first=x[0]
+first.T.todense()
+# %%
+df=pd.DataFrame(first.T.todense(),index=vector.get_feature_names(),columns=["TF-IDF"])
+# %%
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test=train_test_split(x,y,test_size=0.2)
+# %%
+X_train.shape
+# %%
+from sklearn.linear_model import LogisticRegression
+clf=LogisticRegression(random_state=0,multi_class='multinomial')
+# %%
+clf.fit(X_train,y_train)
+# %%
+dt=np.array(['ItsMeSiddharth2000#%!!'])
+pred=vector.transform(dt)
+clf.predict(pred)
+# %%
+y_pred=clf.predict(X_test)
+y_pred
+# %%
+from sklearn.metrics import confusion_matrix,accuracy_score
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+print(accuracy_score(y_test,y_pred))
+# %%
+from sklearn.metrics import classification_report
+print(classification_report(y_test,y_pred))
 
-# %%
-
-# %%
-
-# %%
-
-# %%
-
-# %%
-
-# %%
-
-# %%
